41291b2d759d096d44e064c63f76c9823f231dfc
Karol <93782725+petitess@users.noreply.github.com>
2024-06-09 10:26:20 +0200

    create entra id group, add member

--- New-MgGroup01.ps1
+++ New-MgGroup01.ps1
@@ -2,12 +2,12 @@ $User = 'x-bcb9-7ba91c64e58d'
 $GrpName = 'grp-graph-01'
 
 if (!(Get-MgGroup -Filter "DisplayName eq '$GrpName'")) {
-    Write-Output "Creating grp-graph-01"
-    $GrpId = (New-MgGroup -DisplayName 'grp-graph-01' -MailNickName 'grp-graph-01' -MailEnabled:$False -SecurityEnabled).Id
+    Write-Output "Creating $GrpName"
+    $GrpId = (New-MgGroup -DisplayName $GrpName -MailNickName $GrpName -MailEnabled:$False -SecurityEnabled).Id
     New-MgGroupMember -GroupId $GrpId -DirectoryObjectId $User
 }
 else {
-    Write-Output "grp-graph-01 already exists"
+    Write-Output "$GrpName already exists"
     $GrpId = (Get-MgGroup -Filter "DisplayName eq '$GrpName'").Id
 
     if ($GrpId.Count -gt 1) {
