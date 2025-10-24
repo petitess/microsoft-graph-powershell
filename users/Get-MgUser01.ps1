8d4f54ca4b003ca6d079a96bc7b633d19a7942c1
Karol <93782725+petitess@users.noreply.github.com>
2025-10-24 11:38:41 +0200

    get users with filters

--- users/Get-MgUser01.ps1
+++ users/Get-MgUser01.ps1
@@ -2,3 +2,8 @@ Get-MgUser -All -Property DisplayName, UserPrincipalName, CompanyName `
 -ConsistencyLevel eventual -CountVariable userCount `
 -Filter "startsWith(CompanyName, 'Abc Invest')" |
 Select-Object DisplayName, UserPrincipalName, CompanyName
+
+Get-MgUser -All -Property DisplayName, UserPrincipalName, OfficeLocation `
+-ConsistencyLevel eventual -CountVariable userCount `
+-Filter "startsWith(OfficeLocation, 'A1 Consulting Poland')" |
+Select-Object DisplayName, UserPrincipalName, OfficeLocation
