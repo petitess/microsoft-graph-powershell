a67b8aaa7c3f6a82df2a56dd94c93f3dd75dea1f
Karol <93782725+petitess@users.noreply.github.com>
2025-12-30 15:56:09 +0100

    create entra group

--- groups/groups01.ps1
+++ groups/groups01.ps1
@@ -1,7 +1,9 @@
-Invoke-MgGraphRequest -Uri "/v1.0/groups?$filter=groupId eq '2095c832-2768-4ce9-a248-9815a0f0917d'"
-Invoke-MgGraphRequest -Uri "/v1.0/groups?$filter=startswith(displayName, 'a')&$count=true&$top=1&$orderby=displayName"
-Invoke-MgGraphRequest -Uri "/v1.0/groups?$filter=NOT groupTypes/any(c:c eq 'Unified') and mailEnabled eq true and securityEnabled eq true&$count=true"
-Invoke-MgGraphRequest -Uri "/v1.0/groups?$filter=mailEnabled eq false&securityEnabled eq true"
+Invoke-MgGraphRequest -Uri "/v1.0/groups?`$filter=groupId eq '2095c832-2768-4ce9-a248-9815a0f0917d'"
+Invoke-MgGraphRequest -Uri "/v1.0/groups?`$filter=startswith(displayName, 'a')&`$count=true&`$top=1"
+Invoke-MgGraphRequest -Uri "/v1.0/groups?`$filter=NOT groupTypes/any(c:c eq 'Unified') and mailEnabled eq true and securityEnabled eq true&`$count=true"
+Invoke-MgGraphRequest -Uri "/v1.0/groups?`$filter=mailEnabled eq false&securityEnabled eq true"
+Invoke-MgGraphRequest -Uri "/v1.0/groups?`$orderby=displayName&)"
+Invoke-MgGraphRequest -Uri "/v1.0/groups"
 
 $Body = ConvertTo-Json @{
     description     = "Self help community for library"
