7e353783a813741e12aa661dbf00b17658a39ef6
Karol <93782725+petitess@users.noreply.github.com>
2026-04-16 15:38:53 +0200

    check if MFA is enabled, table output
    Added functionality to retrieve and check authentication methods for users.

--- users/authenticationMethods01.ps1
+++ users/authenticationMethods01.ps1
@@ -1,4 +1,4 @@
-#UserAuthenticationMethod.Read.All 	UserAuthenticationMethod.ReadWrite.All
+#User.Read.All UserAuthenticationMethod.Read.All UserAuthenticationMethod.ReadWrite.All
 $Users = Get-MgUser -Filter "startsWith(displayName, '365-admin')" -All
 Write-Host  "`nRetreived $($users.Count) users";
 
@@ -30,3 +30,22 @@ $Results = $Users | ForEach-Object {
 }
 
 $Results | Format-Table -AutoSize
+
+#Runbook
+Connect-MgGraph -Identity
+
+$Users = (Invoke-MgGraphRequest -Uri "v1.0/users?`$filter=startswith(displayName, '365-admin')").value
+Write-Output  "Retreived $($Users.Count) users";
+
+$Users | ForEach-Object {
+    $Upn = $_.UserPrincipalName
+    $Id  = $_.Id
+    $Nr += 1
+
+    $Methods = (Invoke-MgGraphRequest -Uri "v1.0/users/$Id/authentication/methods").value.'@odata.type'
+
+    $Methods = (Invoke-MgGraphRequest -Uri "v1.0/users/$Id/authentication/methods").value.'@odata.type'
+    $HasAuthenticator = ($Methods -match "#microsoft.graph.microsoftAuthenticatorAuthenticationMethod" ? $true : $false)
+    
+    Write-Output "$Nr.MFA: $HasAuthenticator; Upn: $Upn"
+}
