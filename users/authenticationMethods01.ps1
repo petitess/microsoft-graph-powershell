b1fd73aa916c6eced259932e64bf1448ea606c17
Karol <93782725+petitess@users.noreply.github.com>
2026-04-16 14:59:24 +0200

    check if MFA is enabled
    Retrieve users starting with '365-admin' and check for Microsoft Authenticator methods.

---
+++ users/authenticationMethods01.ps1
@@ -0,0 +1,31 @@
+#UserAuthenticationMethod.Read.All 	UserAuthenticationMethod.ReadWrite.All
+$Users = Get-MgUser -Filter "startsWith(displayName, '365-admin')" -All
+Write-Host  "`nRetreived $($users.Count) users";
+
+$Users | ForEach-Object {
+    $Upn = $_.UserPrincipalName
+    $Id = $_.Id
+
+    $Methods = (Invoke-MgGraphRequest -Uri "v1.0/users/$Id/authentication/methods").value.'@odata.type'
+
+    if ($Methods -match "#microsoft.graph.microsoftAuthenticatorAuthenticationMethod") {
+        Write-Output "$Upn : true"
+    }else {
+        Write-Output "$Upn : false"
+    }
+}
+
+#Table output
+$Results = $Users | ForEach-Object {
+    $Upn = $_.UserPrincipalName
+    $Id  = $_.Id
+
+    $Methods = (Invoke-MgGraphRequest -Uri "v1.0/users/$Id/authentication/methods").value.'@odata.type'
+
+    [pscustomobject]@{
+        UserPrincipalName = $Upn
+        HasAuthenticator  = $Methods -match "#microsoft.graph.microsoftAuthenticatorAuthenticationMethod" ? $true : $false 
+    }
+}
+
+$Results | Format-Table -AutoSize
