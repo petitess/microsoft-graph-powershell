76590cb8b6c0b1c54aa996fbf3f4967ebd95f0b3
Karol <93782725+petitess@users.noreply.github.com>
2026-04-24 16:25:41 +0200

    create/upload new folder/file
    This script interacts with Microsoft Graph to manage SharePoint site drives and files, including creating a new folder and uploading a markdown file.

---
+++ sharepoint/drive02.ps1
@@ -0,0 +1,31 @@
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/root" )
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/companyname.sharepoint.com" )
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/companyname.sharepoint.com:/sites/ADL-B3" )
+
+$siteId = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/root:/sites/ADL-A3" ).id
+
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive" ).name
+
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive")
+
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/root/children").value
+
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/01RRNAIB7RYVUGBUMQEJAKG7QJUIJIYR2B/children").value
+
+$Body = @{
+    name = "new_folder"
+    folder = @{}
+    "@microsoft.graph.conflictBehavior" = "rename" #fail, replace
+}
+
+(Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/01RRNAIB7RYVUGBUMQEJAKG7QJUIJIYR2B/children" -Body $Body)
+
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/01RRNAIB7Q67ULQW52SVDKAQQLVXSSF5SG/content" -OutputFilePath ./test.md)
+
+$Content = @"
+# New File
+
+This is the content of the new file.
+"@
+
+(Invoke-MgGraphRequest -Method PUT -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/01RRNAIB7RYVUGBUMQEJAKG7QJUIJIYR2B:/new_file.md:/content" -Body $Content -ContentType "text/markdown")
