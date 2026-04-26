730b82e6d6767cde6b6018d66332de35b21bdae5
Karol <93782725+petitess@users.noreply.github.com>
2026-04-26 05:37:03 +0200

    download files from root/new library
    Refactor SharePoint drive access to download files from specified folders.

--- sharepoint/drive01.ps1
+++ sharepoint/drive01.ps1
@@ -7,14 +7,23 @@
 
 $siteId = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/root:/sites/ADL-A3" ).id
 
-(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive" ).name
-
-(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive")
-
-(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/root/children").value
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive").name
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drives").name
+#Download from root library
+$FolderId = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/root/children?`$filter=name eq 'new_sys'").value.id
+$Files = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/$FolderId/children").value
+$Files | ForEach-Object {
+    (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/$($_.id)/content" -OutputFilePath "./$($_.name)")
+}
 
 (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/01RRNAIB7RYVUGBUMQEJAKG7QJUIJIYR2B/children").value
 
-(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/01RRNAIB7Q67ULQW52SVDKAQQLVXSSF5SG/content" -OutputFilePath ./test.md)
+(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drive/items/$FolderId/content" -OutputFilePath ./test.md)
+#Download from new library
+$DocId = ((Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drives").value | Where-Object { $_.name -eq "SYSTEM" }).id
+$FolderId = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drives/$DocId/items/root/children?`$filter=name eq 'customerX'").value.id
+$Files = (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drives/$DocId/items/$FolderId/children").value
 
-(Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drives")
+$Files | ForEach-Object {
+    (Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/drives/$DocId/items/$($_.id)/content" -OutputFilePath "./$($_.name)")
+}
