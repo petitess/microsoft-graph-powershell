918b2fff429814adb21a4327d5d79f58b789e3ed
Karol <93782725+petitess@users.noreply.github.com>
2026-06-25 15:00:29 +0200

    create/update an entra id group
    Updated script to create groups dynamically from an array of custom objects with specific properties.

--- groups/groups01.ps1
+++ groups/groups01.ps1
@@ -5,18 +5,25 @@ Invoke-MgGraphRequest -Uri "/v1.0/groups?`$filter=mailEnabled eq false&securityE
 Invoke-MgGraphRequest -Uri "/v1.0/groups?`$orderby=displayName&)"
 Invoke-MgGraphRequest -Uri "/v1.0/groups"
 
-$Body = ConvertTo-Json @{
-    description     = "Self help community for library"
-    displayName     = "Library Assist"
-    groupTypes      = @(
-        "Unified"
-    )
-    mailEnabled     = $true
-    mailNickname    = "library"
-    securityEnabled = false
-}
+$groups = @(
+    [PSCustomObject]@{ Name = "grp-dev-sys1-user-PIM-DEV"; Des = "This is a dev group" }
+    [PSCustomObject]@{ Name = "grp-dev-sys1-user-PIM-TEST"; Des = "This is a test group" }
+) 
 
-$Script = Invoke-MgGraphRequest -Uri "/v1.0/groups" -Method POST -Body $Body 
+$groups | ForEach-Object {
+    $newName = $_.Name
+    $newName
+    $Body = ConvertTo-Json @{
+        description     = $_.Des
+        displayName     = $newName
+        groupTypes      = @()
+        mailEnabled     = $false
+        mailNickname    = $newName
+        securityEnabled = $true
+        uniqueName      = $newName
+    }
+    Invoke-MgGraphRequest -Uri "/v1.0/groups" -Method POST -Body $Body
+}
 #PATCH uniqueName can be udated only once, immutable
 $groups = @(
     [PSCustomObject]@{ Name = "grp-dev-sys1-user-PIM-DEV"; Id = "2095c832-2768-4ce9-a248-9815a0f0917d" }
