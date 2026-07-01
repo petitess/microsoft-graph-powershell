906c30c8d1a9939fe5b1ceebe056170c57b73b94
Karol <93782725+petitess@users.noreply.github.com>
2026-07-01 15:37:37 +0200

    list users, pagination

--- users/users01.ps1
+++ users/users01.ps1
@@ -1,8 +1,10 @@
-$Uri = "/v1.0/users"
+$Uri = "/v1.0/users?$filter=memberType eq 'Member'&$select=id,displayName,userPrincipalName,createdDateTime&$top=999"
+$Uri = "/v1.0/users?`$filter=userType eq 'Member' and OnPremisesSyncEnabled eq true"
 $Users = @()
 do {
     $Response = Invoke-MgGraphRequest -Uri $Uri
     $Users += $Response.value
     $Uri = $Response.'@odata.nextLink'
 } while ($Uri)
-$Users
+$Users.Count
+$Users[0] | ConvertTo-Json -Depth 10
